a79a9db18194ad362884d8f7b568b85205ea1fe9
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-12 01:38:32 -0800

    Finish part 9

--- anyone_react/src/App.js
+++ anyone_react/src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
-import MyName from './MyName';
 import './App.css';
+import Counter from './Counter'
 
 class App extends Component {
 
 
   render() {
 
-    const name = "Haram";
     return (
       <div className="App" >
-        <div>김하람의 영어 이름 : <MyName name="기무"/>
-        </div>
-        <div>김낸뚜</div>
-        <div>
-          {
-            (function() {
-              if (name === "Haram") return "Haramie"
-              if (name === "Yain") return "Ggang Pae"
-              if (name === "Harams") return "Haramzzing"
-              return "없다"
-            })()
-          }
-        </div>
+        <Counter/>
       </div>
 
     );
