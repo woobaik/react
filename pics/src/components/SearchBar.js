666a395d802feafe2d8a10b7ff83fa1065ff6c50
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-15 16:34:06 -0800

    Initialize pics project

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+
+class SearchBar extends Component {
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  render(){
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>User Input</label>
+            <input type="text" onChange={this.onInputChange}/>
+          </div>
+        </form>
+      </div>
+    );
+  }
+};
+
+export default SearchBar;
