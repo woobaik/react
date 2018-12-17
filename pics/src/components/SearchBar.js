029adfff3816e1aee61f12c7cb216a0750e331d3
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-16 19:39:53 -0800

    Finish chapter 7

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -4,17 +4,27 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
-  onInputChange(event) {
-    console.log(event.target.value);
+  state = {
+    inputValue: ''
+  }
+
+  onFormSubmit = (event) =>  {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.inputValue)
   }
 
   render(){
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>User Input</label>
-            <input type="text" onChange={this.onInputChange}/>
+            <input
+              type="text"
+              value={this.state.inputValue }
+              onChange={e => this.setState({inputValue: e.target.value})}
+            />
           </div>
         </form>
       </div>
