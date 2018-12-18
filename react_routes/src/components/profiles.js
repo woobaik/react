05e943cc5ae5b4f0bfff309a7661a205b0ff8263
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-18 13:42:15 -0800

    Finish chap1

---
+++ react_routes/src/components/profiles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profiles = () => {
+  return (
+    <div>
+      Profiles
+    </div>
+  )
+
+}
+
+export default Profiles
--- react_routes/src/index.js
+++ react_routes/src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+
+//Components
+
+import Home from './components/home';
+import Posts from './components/posts';
+import Profiles from './components/profiles';
+
+
+const App = () => {
+  return (
+      <BrowserRouter>
+        <div>
+          <div>
+            Header
+          </div>
+          <Route path="/" exact component={Home} />
+          <Route path="/posts" component={Posts}/>
+          <Route path="/profile" component={Profiles} />
+        </div>
+      </BrowserRouter>
+  )
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
