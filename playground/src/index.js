e406c9e585b3b0e65eb3ea1add0c4ca55d836fc4
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-19 15:08:50 -0800

    Finish transporting function to class

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,60 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {name: 'Haram', age: 29}
+  }
 
   render() {
     return (
       <div>
-        <Header username='haram'/>
-        <Greeting />
+        <div>{this.state.name}</div>
+        <div>{this.state.age}</div>
       </div>
     )
   }
 }
 
-class Header extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <header>
-        <p>Welcome, {this.props.username}</p>
-      </header>
-    )
-  }
-}
-
-class Greeting extends Component {
-
-
-  render() {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeofDay
-
-    if (hours < 12) {
-      timeofDay = 'morning'
-    } else if  (hours >=12 && hours < 17) {
-      timeofDay = "afternoon"
-    } else {
-      timeofDay = 'night'
-    }
-    return <div>Good {timeofDay} to you, sir or madam!</div>
-  }
-}
-
-
-
-
-
-
-
 ReactDOM.render(<App /> , document.querySelector('#root'))
-
-if (module.hot) {
-  module.hot.accept()
-}
