a5ae1da1396cdffa5e13fa700cce50f72656b70e
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-20 11:44:41 -0800

    Use this as a reference

---
+++ playground/src/TodoItem.js
@@ -0,0 +1,19 @@
+import React,{ Component } from 'react';
+
+
+class TodoItem extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        apple
+      </div>
+    )
+  }
+}
+
+export default TodoItem
--- playground/src/index.js
+++ playground/src/index.js
@@ -1,20 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoItem from './TodoItem'
 
-class App extends Component {
+class App extends React.Component {
+
   constructor(props) {
     super(props)
-    this.state = {name: 'Haram', age: 29}
+    this.state = {
+                  list: {
+                      item1: { id: 1,
+                               title: 'Zico is old enough.',
+                               favorite: "true"},
+
+                      item2: { id :2,
+                               title: 'React is hard to learn.',
+                               favorite: "false"},
+
+                      item3 : { id: 3,
+                               title: 'I am hungry haha',
+                               favorite: "true"}
+                  },
+                }
   }
 
   render() {
-    return (
+    const listss = this.state.list.map(item => <TodoItem/>)
+
+    return(
       <div>
-        <div>{this.state.name}</div>
-        <div>{this.state.age}</div>
+        { listss }
       </div>
     )
   }
 }
 
-ReactDOM.render(<App /> , document.querySelector('#root'))
+
+ReactDOM.render(<App />, document.querySelector('#root'))
+
+if (module.hot) {
+  module.hot.accept();
+}
