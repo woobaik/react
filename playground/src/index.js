bcdd2b9864e3a21874dee5a44d130f6cb21bf86c
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-22 17:02:26 -0800

    Used Axios

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import axios from 'axios';
 
 //Components
 
@@ -9,10 +10,17 @@ import CommentDetail from './CommentDetail'
 
 class App extends React.Component {
 
-  render() {
 
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term }
+      headers: { Authorization: 'Client-ID 8525e3c123cdc5072887b640ef000f8077610996ef8eeea3d2708f22e298af51'
+      }
+    });
+  }
+  render() {
     return (
-      <div className="ui comments">
+      <div className="ui comments">``
         <CommentDetail avatar={faker.internet.avatar()}
                        userName={faker.internet.userName()}
                        commentText={faker.lorem.sentence()}/>
@@ -30,3 +38,4 @@ ReactDOM.render(<App />, document.querySelector('#root'))
 if (module.hot) {
   module.hot.accept()
 }
+`````
