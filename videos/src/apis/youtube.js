a1bc0c499c8bced61cfe6bed77c5d18400d603fc
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-01-02 18:42:32 -0800

    mockup search bar and implemented Youtube dashboard

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,1 @@
+const KEY = 'AIzaSyDgjIxV8C-MujBqyfRzxSoNbSCSPhqRwOE ';
---
+++ videos/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//Components
+
+import SearchBar from './SearchBar'
+
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+}
+
+
+export default App
