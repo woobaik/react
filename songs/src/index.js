9daf9283302b4b82eed53eb7c9aed2241c88314e
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-01-05 22:07:56 -0800

    Can\'t find error on this page

--- songs/src/index.js
+++ songs/src/index.js
@@ -7,8 +7,6 @@ import App from './components/App';
 import reducers from './reducers/';
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
-  </Provider >
+
   , document.querySelector('#root')
 );
