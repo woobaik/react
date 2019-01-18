d91b94e350218f643b85399e28b5e6d610c38196
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-01-17 23:54:37 -0800

    Add fetch user action

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -7,3 +7,11 @@ export const fetchPosts = ()  => {
     dispatch({type: 'FETCH_POSTS', payload: response.data});
   }
 };
+
+export const fetchUser = () => {
+  return async function(dispatch) {
+    const response = await  jsonPlaceholder.get('/user')
+
+    dispatch({type: 'FETCH_USER, payload: response.data'})
+  }
+}
