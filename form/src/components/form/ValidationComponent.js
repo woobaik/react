dd6ef1a2a27aeef158bffd8aebc580b5d2e0fcb3
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-29 19:14:28 -0700

    Finish 3rd task validation message

--- form/src/components/form/ValidationComponent.js
+++ form/src/components/form/ValidationComponent.js
@@ -1,7 +1,21 @@
 import React from "react"
 
 const validation = props => {
-  return <div>Length : {props.length}</div>
+  let lengthValidationMsg = null
+  if (props.length === 0) {
+    lengthValidationMsg = null
+  } else if (props.length < 5) {
+    lengthValidationMsg = <div>text is too short!</div>
+  } else {
+    lengthValidationMsg = <div>Text is long enough</div>
+  }
+
+  return (
+    <div>
+      <div>Length : {props.length}</div>
+      {lengthValidationMsg}
+    </div>
+  )
 }
 
 export default validation
