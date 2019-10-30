4cf6f4fc61a6af7138637cccd609f6e8aaf913d8
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-29 19:00:18 -0700

    Finish first task. input and input length

--- form/src/App.js
+++ form/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    input: "",
+    length: 0
+  }
+
+  handleChange = event => {
+    this.setState({
+      input: event.target.value,
+      length: event.target.value.length
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <label for='input'>Text input</label>
+        <input name='input' id='input' onChange={this.handleChange}></input>
+        <p>{this.state.length}</p>
+        <p>{this.state.input}</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
