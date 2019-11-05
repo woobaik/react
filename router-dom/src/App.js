123c69ab7f6728c00978a97a4e5f85f6f01d3750
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-11-04 20:05:26 -0800

    Finish first example

--- router-dom/src/App.js
+++ router-dom/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom"
+import "./App.css"
+import Home from "./components/Home"
+import About from "./components/About"
+import User from "./components/User"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='App'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/home'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/user'>User</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/about'>
+            <About></About>
+          </Route>
+          <Route path='/user'>
+            <User />
+          </Route>
+          <Route path='/'>
+            <Home></Home>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
